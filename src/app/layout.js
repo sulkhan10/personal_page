b8bb9d212d395e342e7b9aa3978475d8be32f77f
sulkhan10 <sulkhangalangsakti@gmail.com>
2026-01-14 21:38:26 +0700

    feat : add contents

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,16 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Press_Start_2P, VT323 } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const pressStart = Press_Start_2P({
+  variable: "--font-retro",
   subsets: ["latin"],
+  weight: "400",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const vt323 = VT323({
+  variable: "--font-console",
   subsets: ["latin"],
+  weight: "400",
 });
 
 export const metadata = {
@@ -20,7 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${pressStart.variable} ${vt323.variable} antialiased`}
       >
         {children}
       </body>
